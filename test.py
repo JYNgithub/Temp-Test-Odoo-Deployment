bf01bba2a2e2d14f701ae5b6db5592b3014d5f8b
JYNgithub <chongjinjye@gmail.com>
2025-08-27 21:23:59 +0800

    fix

--- test.py
+++ test.py
@@ -7,3 +7,7 @@ db_user = os.environ.get('DB_USER')
 db_password = os.environ.get('DB_PASSWORD')
 db_name = os.environ.get('DB_NAME')
 db_sslmode = os.environ.get('DB_SSLMODE')
+
+"""
+python3 odoo-bin --db_host="$DB_HOST" --db_port="$DB_PORT" --db_user="$DB_USER" --db_password="$DB_PASSWORD" --database="$DB_NAME" --db_sslmode="$DB_SSLMODE"
+"""
