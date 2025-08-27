a089e0cc8e777ed5667470fde0d0bb43c430074f
JYNgithub <chongjinjye@gmail.com>
2025-08-27 20:38:34 +0800

    fix

---
+++ odoo.py
@@ -0,0 +1,9 @@
+import os
+
+admin_passwd = os.environ.get('ADMIN_PASSWD')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_sslmode = os.environ.get('DB_SSLMODE')
